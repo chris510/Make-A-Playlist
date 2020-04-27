8d99f6df78635790200c5c0202b558e854c43c51
Chris Trinh <christrinh5@gmail.com>
2020-04-26 18:45:08 -0700

    Create a list of artist name and track name

--- frontend/src/pages/about/about.component.tsx
+++ frontend/src/pages/about/about.component.tsx
@@ -5,10 +5,10 @@ const About = () => {
   return (
     <div className="about">
       <div className="content">
-      <h1>APP NAME HERE</h1>
+      <h1>Make a Playlist!</h1>
         <div className="content-inner">
-          <p>Welcome to APP NAME HERE, a web app that allows you to make a Spotify playlist full of your favorite songs by your favorite artist and artist genres that are similar to them! Just enter the name of the artist below and click Make Playlist!</p>
-          <p>If you also love the playlist that was generated with this app, you can choose to send it to your phone! Just enter your phone number and click Send!</p>
+          <p>Welcome to APP NAME HERE, a web app that allows you to make a Spotify playlist full of your favorite songs by your favorite artist and artist genres that are similar to them! Just enter the name of the artist below and click the play button! An algorithm was created using Spotify's own API to create a playlist of songs of the desired artist as well as songs of related artist!</p>
+          <p>If you also love the playlist that was generated with this app, you can choose to send it to your phone! Just click the message icon in the playlist section, type in your phone number, and click send!</p>
         </div>
       </div>
     </div>
--- frontend/src/pages/home/home.component.tsx
+++ frontend/src/pages/home/home.component.tsx
@@ -7,13 +7,15 @@ import SearchButton from '../../components/search-button/search-button.component
 const Home = () => {
   return ( 
     <div className="home">
-      <h1 className="header-1">Music for Everyone.</h1>
-      <h2 className="header-2">Discover and share</h2>
-      <h3 className="header-3">your perfect</h3>
-      <h4 className="header-4">sound.</h4>
-      <Link to="/playlist">
-        <SearchButton/>
-      </Link>
+      <div className="content">
+        <h1 className="header-1">Music for Everyone.</h1>
+        <h2 className="header-2">Discover and share</h2>
+        <h3 className="header-3">your perfect</h3>
+        <h4 className="header-4">sound.</h4>
+        <Link to="/playlist">
+          <SearchButton/>
+        </Link>
+      </div>
     </div>
   )
 }
