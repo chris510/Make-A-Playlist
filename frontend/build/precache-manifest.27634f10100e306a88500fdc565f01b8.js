0ad9c49ced77f5a17f8614e2b7eab45d6c5028c0
Chris Trinh <christrinh5@gmail.com>
2020-04-27 20:38:40 -0700

    Add build folder

---
+++ frontend/build/precache-manifest.27634f10100e306a88500fdc565f01b8.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0d8e52483f397460e7a2e15f7533ae95",
+    "url": "/index.html"
+  },
+  {
+    "revision": "17c4e9f8543e55f2ba1f",
+    "url": "/static/css/main.285ddf5e.chunk.css"
+  },
+  {
+    "revision": "b33567a8fbeda10361e1",
+    "url": "/static/js/2.d5a0fb82.chunk.js"
+  },
+  {
+    "revision": "7142d6eb82b8b4e023a6553002ad5868",
+    "url": "/static/js/2.d5a0fb82.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "17c4e9f8543e55f2ba1f",
+    "url": "/static/js/main.c7de2740.chunk.js"
+  },
+  {
+    "revision": "700d31d1c6f37b7bdc51",
+    "url": "/static/js/runtime-main.84578d5e.js"
+  },
+  {
+    "revision": "318f9f8e9bd4909f2e659bc26abbc8bc",
+    "url": "/static/media/neon-background.318f9f8e.jpeg"
+  }
+]);
