7ea8fe81aab5c0ef8589fed502e8e80efe567ece
Chris Trinh <christrinh5@gmail.com>
2020-04-27 19:45:48 -0700

    Use gunicorn

--- app.py
+++ app.py
@@ -85,6 +85,6 @@ api.add_resource(Message, "/message")
 
 # app.run(port=5000, debug=True)
 if __name__ == '__main__':
-  app.run(port=5000)
+  app.run()
 
 
--- frontend/src/pages/about/about.component.jsx
+++ frontend/src/pages/about/about.component.jsx
@@ -7,7 +7,7 @@ const About = () => {
       <div className="content">
       <h1>Make a Playlist!</h1>
         <div className="content-inner">
-          <p>Welcome to APP NAME HERE, a web app that allows you to make a Spotify playlist full of your favorite songs by your favorite artist and artist genres that are similar to them! Just enter the name of the artist below and click the play button! An algorithm was created using Spotify's own API to create a playlist of songs of the desired artist as well as songs of related artist!</p>
+          <p>Welcome to Make a Playlist, a web app that allows you to make a Spotify playlist full of your favorite songs by your favorite artist and artist genres that are similar to them! Just enter the name of the artist below and click the play button! An algorithm was created using Spotify's own API to create a playlist of songs of the desired artist as well as songs of related artist!</p>
           <p>If you also love the playlist that was generated with this app, you can choose to send it to your phone! Just click the message icon in the playlist section, type in your US phone number, and click send!</p>
         </div>
       </div>
