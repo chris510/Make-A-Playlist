6f9e6da3bcdf4015b566f69cf819df67565bec62
Chris Trinh <christrinh5@gmail.com>
2020-05-11 21:07:40 -0700

    Moible background change

--- frontend/build/precache-manifest.64247c401b55e866c0838cf8a0bd5a69.js
+++ frontend/build/precache-manifest.64247c401b55e866c0838cf8a0bd5a69.js
@@ -1,11 +1,11 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "26ad211fca76f5d52ef6d5aa1618cbc0",
+    "revision": "ea2646049cd8e1b6a20717864a0c4f19",
     "url": "/index.html"
   },
   {
-    "revision": "0699eefe36271097f075",
-    "url": "/static/css/main.934f9e49.chunk.css"
+    "revision": "e647d693168be7f587ed",
+    "url": "/static/css/main.092639f7.chunk.css"
   },
   {
     "revision": "4a7a5cd7021b547696cb",
@@ -16,7 +16,7 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/static/js/2.f6499cda.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "0699eefe36271097f075",
+    "revision": "e647d693168be7f587ed",
     "url": "/static/js/main.dcba0fc3.chunk.js"
   },
   {
--- frontend/build/service-worker.js
+++ frontend/build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.c4277bb508e7feb71bc9a78cfcbdb0a9.js"
+  "/precache-manifest.64247c401b55e866c0838cf8a0bd5a69.js"
 );
 
 self.addEventListener('message', (event) => {
